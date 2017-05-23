f76104a69a4e512ea8369431f32a5aaa2a2292ea
Mark O'Connell <moconnell@ucd.ie>
2017-05-23 11:15:41 +0100

    allow failure of batches, status bar etc

--- R/mbcbigp.R
+++ R/mbcbigp.R
@@ -27,13 +27,16 @@
 mbcbigp <-
 function (x, groups = 2, batches = 3, batchindex = NULL, batchsize = NULL,
   maxiter = 50, plot = FALSE, likelihood = FALSE, verbose = TRUE, abstol = 1e-3,
-  method_sigma_AB = c("numeric", "analytic"), z = NULL)
+  method_sigma_AB = c("numeric", "analytic"), z = NULL, scorefun = criteria_1)
 {
-  method_signa_AB <- match.arg(method_sigma_AB)
+  method_sigma_AB <- match.arg(method_sigma_AB)
   set.seed(13432523)
   x <- data.matrix(x)
   N <- nrow(x)
   p <- ncol(x)
+
+  if (!is.null(batchsize))
+    batches <- round(p / batchsize)
 
   if (is.null(batchindex)){
     if ((lb <- length(batches)) < 2){
@@ -52,35 +55,67 @@ function (x, groups = 2, batches = 3, batchindex = NULL, batchsize = NULL,
   #     parameters = NULL, groups = groups)
   #}
 
-  ## Do the first batch using marginal density.
+  ## Do the first batch using regular model-based clustering.
 
   if (verbose)
     cat("\nBatch 1 \n-------------------------------------------")
+  else {
+    pb <- txtProgressBar(min = 0, max = Q, style = 3)
+  }
+
+  success <- score <- rep(NA, Q)
 
   batch <- list()
   batch[[1L]] <- mbc(x = x[, batchindex[[1L]]], groups = groups, maxiter =
     maxiter, likelihood = likelihood, verbose = verbose, plot = plot)
+  success[1L] <- 1
+
+  score[1L] <- scorefun(mean = batch[[1]]$mean, sigma = batch[[1]]$sigma,
+    groups = groups)
+  setTxtProgressBar(pb, 1)
 
   ## Other batches
 
-  for (q in 2:Q){
+  qprev <- 1 ## indexes 'batchindex', becomes 'q' on successful iteration.
+  q <- ## indexes 'batchindex', advances on each iteration.
+  qindex <- 2 ## indexes 'batch', advances on each successful iteration.
+
+  while (q <= Q){
 
     if (verbose)
       cat("\nBatch", q,"\n-------------------------------------------\n")
 
-    batch[[q]] <- mbc_cond(x_A = x[, batchindex[[q - 1L]]],
-      x_B = x[, batchindex[[q]]], mean_A = batch[[q - 1L]]$mean,
-      sigma_AA = batch[[q - 1L]]$sigma, z = batch[[q - 1L]]$z, pro =
-      batch[[q - 1L]]$pro, groups = groups, maxiter = maxiter, likelihood =
-      likelihood, verbose = verbose, plot = plot, abstol = abstol,
-      method_sigma_AB = method_sigma_AB)
-
+    tryobj <- try({
+      newz <- batch[[qindex - 1L]]$z
+      #newz <- initialise.memberships(x = x[, batchindex[[q]]], groups = groups)
+      batch[[qindex]] <- mbc_cond(x_A = x[, batchindex[[qprev]]],
+        x_B = x[, batchindex[[q]]], mean_A = batch[[qindex - 1L]]$mean,
+        sigma_AA = batch[[qindex - 1L]]$sigma, z = newz, pro =
+        batch[[qindex - 1L]]$pro, groups = groups, maxiter = maxiter, likelihood
+        = likelihood, verbose = verbose, plot = plot, abstol = abstol,
+        method_sigma_AB = method_sigma_AB)
+    }, silent = FALSE)
+    if (identical(class(tryobj), "try-error")){
+      warning("batch ", q, " failed")
+    } else {
+      score[qindex] <- scorefun(mean = batch[[qindex]]$mean, sigma =
+        batch[[qindex]]$sigma, groups = groups)
+      success[qindex] <- q
+      qprev <- q
+      qindex <- qindex + 1L
+    }
+    setTxtProgressBar(pb, q)
+    q <- q + 1L
   }
   z <- lapply(batch, function (x) x$z)
   zave <- Reduce(`+`, z) / length(z)
   batchindexnames <- if (!is.character(batchindex[[1]]))
     lapply(batchindex, function (tmp) colnames(x)[tmp])
-  batchindex
+  if (verbose){
+    cat(paste0("\n\nFinished. Processed ", length(batch), "/", Q, " batches (",
+      round(100 * length(batch) / Q),"%)\n\n"))
+  } else close(pb)
   invisible(structure(list(z = zave, batch = batch, batchindex =
-    batchindexnames, groups = groups), class = "mbcbigp"))
+    batchindexnames, success = success[!is.na(success)], score =
+    score[!is.na(score)], groups = groups), class = "mbcbigp"))
 }
