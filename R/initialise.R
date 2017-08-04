b7130f6deaf231e669c7eba2d08c7cf50b41c336
Mark O'Connell <mark_ajoc@yahoo.ie>
2017-08-04 13:01:20 +0100

    tidy up initialisation

--- R/initialise.R
+++ R/initialise.R
@@ -5,21 +5,33 @@
 #'
 #' @param x A data frame containing continuous data.
 #' @param groups The number of groups.
-#' @param method The method to give initial clustering.
+#' @param method The method to give initial clustering, currently available:
+#'   \code{"kmeans"} and \code{"hclust"}.
+#' @param ... Arguments to pass to clustering functions.
 #'
-#' @return A \code{groups} x \code{nrow(x)} matrix of 0/1 entries denoting cluster
-#'   membership.
+#' @return A \code{groups} x \code{nrow(x)} matrix of 0/1 entries denoting
+#'   cluster membership.
 #'
 #' @examples
 #' data(mtcars)
 #' initialise.memberships(mtcars, 3)
 
 initialise.memberships <-
-function (x, groups, method = c("kmeans", "hclust"))
+function (x, groups, method = c("kmeans", "hclust", "mclust"), ...)
 {
   x <- x[, apply(x, 2, function(obj) !any(is.na(obj))), drop = FALSE]
-  cl <- stats::kmeans(x, groups)
-  z <- model.matrix(~ as.factor(cl$cluster) - 1)
+  method <- match.arg(method)
+  if (identical(method, "kmeans")){
+    cl <- stats::kmeans(x, groups, ...)
+    clustering <- cl$cluster
+  } else if (identical(method, "hclust")){
+    d <- dist(x)
+    h <- hclust(d, ...)
+    clustering <- cutree(h, k = groups)
+  } else if (identical(method, "mclust")){
+    stop("initialisation method 'mclust' not currently supported")
+  } else stop("initialisation method '", method, "' not recognised")
+  z <- model.matrix(~ as.factor(clustering) - 1)
   colnames(z) <- 1:groups
   z[, , drop = FALSE]
 }
